e76c880a1a1354d47ccba5ab9e28796a444d1db8
Gianfranco Palumbo <gianpa@gmail.com>
2019-10-28 14:20:02 +0000

    add orders schema

--- models/orders.js
+++ models/orders.js
@@ -1,32 +1,100 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema({
-  'buyer': mongoose.Schema.Types.ObjectId,
-  'buyerType': String,
-  'cityRecipient': String,
-  'citySender': String,
-  'createdAt': Date,
-  'currency': String,
-  'dateCancelled': Date,
-  'dateCompleted': Date,
-  'dateConfirmed': Date,
-  'dateDelivered': Date,
-  'datePaid': Date,
-  'datePending': Date,
-  'dateShipped': Date,
-  'product': mongoose.Schema.Types.ObjectId,
-  'reason': String,
-  'reviewFromBuyer': mongoose.Schema.Types.ObjectId,
-  'reviewFromSeller': mongoose.Schema.Types.ObjectId,
-  'seller': mongoose.Schema.Types.ObjectId,
-  'shippingProvider': String,
-  'shippingStatus': String,
-  'shippingUpdatedAt': Date,
-  'status': String,
-  'trackingNumber': String,
-  'transactionId': String,
-  'transactionStatus': String,
-  'updatedAt': Date,
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  buyer: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  buyerType: {
+    type: String,
+    required: true,
+    enum: ['User', 'UserWeb'],
+  },
+  cityRecipient: String,
+  citySender: String,
+  createdAt: Date,
+  currency: {
+    type: String,
+    required: true,
+    default: 'UAH',
+  },
+  dateCancelled: Date,
+  dateCompleted: Date,
+  dateConfirmed: Date,
+  dateDelivered: Date,
+  datePaid: Date,
+  datePending: Date,
+  dateShipped: Date,
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  reason: String,
+  reviewFromBuyer: Schema.Types.ObjectId,
+  reviewFromSeller: Schema.Types.ObjectId,
+  seller: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  },
+  shippingProvider: {
+    type: String,
+    enum: ['novaposhta'],
+  },
+  shippingStatus: String,
+  shippingUpdatedAt: Date,
+  status: {
+    type: String,
+    required: true,
+    default: 'pending',
+    enum: [
+      // Unpaid - Customer started the checkout process. Payment is not completed.
+      'pending',
+
+      // (1)
+      // Buyer pays and waiting for seller to confirm – One quantity of the Product is now in carted array
+      'paid',
+
+      // (1)
+      // Product is ready for shipment. Tracking number is generated automatically
+      'confirmed',
+
+      // (2)
+      'shipped',
+
+      // Seller cancels order. Requires reason.
+      // or
+      // Buyer cancels order (or doesn't pay in 15 mins). Reason if internal process (payment denied/timeout)
+      'cancelled',
+
+      // (2)
+      'delivered',
+
+      // (2)
+      // Item has been collected
+      'completed',
+
+      // (1)
+      // Buyer fails to collect
+      // or
+      // Buyer refuses the item (not as described)
+      'failed_by_buyer',
+
+      // (1) or Escrow Manager
+      // Seller fails to ship
+      // or
+      // Seller doesn't confirm order
+      'failed_by_seller',
+    ],
+  },
+  trackingNumber: String,
+  transactionId: String,
+  transactionStatus: String,
+  updatedAt: Date,
 }, {
   timestamps: false,
 });
--- models/products.js
+++ models/products.js
@@ -12,15 +12,15 @@ const schema = new Schema({
     type: [Number],
     required: true,
   },
-  'uuid': String,
-  'createdAt': Date,
-  'currency': String,
-  'description': String,
+  uuid: String,
+  createdAt: Date,
+  currency: String,
+  description: String,
   dropId: {
     type: Schema.Types.ObjectId,
     ref: 'drops',
   },
-  'locality': String,
+  locality: String,
   photoURIs: {
     type: [String],
     // required: true, // added async after the images are uploaded to GSC
@@ -29,16 +29,16 @@ const schema = new Schema({
     type: Schema.Types.Decimal128,
     required: true,
   },
-  'quantity': Number,
-  'reservedDate': Date,
+  quantity: Number,
+  reservedDate: Date,
   status: {
     type: String,
     required: true,
     default: 'forsale',
     enum: ['forsale', 'sold', 'banned', 'deleted', 'ready'],
   },
-  'updatedAt': Date,
-  'weight': Number,
+  updatedAt: Date,
+  weight: Number,
 }, {
   timestamps: false,
 });
